6407af52e5481d86d69d0e1876ed8828b021d279
kwthr <k.mahfoodh2002@gmail.com>
2023-12-31 13:55:21 +0300

    Reservation CRUD

---
+++ stadiumspotter/src/components/reservation/Reservation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Reservation(props) {
+  return (
+    <>
+    {/* <td></td> */}
+    <td>{props.date}</td>
+    <td>{props.startTime}</td>
+    <td>{props.endTime}</td>
+    <td>{props.Status}</td>
+    <td>{props.price}</td>
+    <td>{props.user}</td>
+    <td>{props.stadiumName}</td>
+    <td><button onClick={ () => props.edit(props._id) }>Edit</button></td>
+    </>
+  )
+}
